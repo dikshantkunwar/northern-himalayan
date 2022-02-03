0fc9d4d0551b0ccc330af92761d0a6011ceb9b02
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-02-03 14:06:58 -0500

    Created about, menu and contact pages with empty stubs and added navigation and its styles and routed the pages

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Header.module.scss'
+
+export default function Header() {
+  return (
+    <header className={styles.root}>
+      <div>
+        <h1><a href='/'>Northern Himalayan Café</a></h1>
+      </div>
+
+      <div>
+        <a href='/about'>About</a>
+        <a href='/menu'>Menu</a>
+        <a href='/contact'>Contact</a>
+      </div>
+    </header>
+  )
+}
