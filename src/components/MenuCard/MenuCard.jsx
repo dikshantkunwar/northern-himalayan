9d6638474dd02edc9c43f1f9fc4a8a209a7a32df
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-02-04 13:21:12 -0500

    Created menu data to populate menu card with items

---
+++ src/components/MenuCard/MenuCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import MenuItem from '../MenuItem';
+import styles from './MenuCard.module.scss';
+
+export default function MenuCard(props) {
+
+  const menuItems = props.menuItems.map( (item) => {
+    if (item.menuType === props.cardHeader) {
+      return (
+      <MenuItem 
+        itemName={item.itemName} 
+        itemPrice={item.itemPrice} 
+        itemDescription={item.description}
+      />)
+    }
+  })
+  return (
+    <div className={styles.menuCard}>
+      <h3>{props.cardHeader}</h3>
+      {menuItems}
+    </div>
+  )
+}
